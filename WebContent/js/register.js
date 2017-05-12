de1f2208fd3805c363286a494132b5b9314bba19
as777 <pson6134@uni.sydney.edu.au>
2017-05-13 00:24:06 +1000

    Initial commit

---
+++ WebContent/js/register.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	
+	$("#submitbutton").bind("click", function() {
+		
+		var password = $("#password").val();
+		var passwordcfm = $('#passwordcfm').val();
+		
+		if (password != passwordcfm) {
+			alert("wrong password confirm");
+		} else {
+			$("#registerform").submit();
+		}
+		
+	});
+});

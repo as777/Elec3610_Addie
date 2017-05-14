dd9875feb2fd236c61da50c9659b34abad8ad8e7
as777 <pson6134@uni.sydney.edu.au>
2017-05-14 23:33:39 +1000

    register page;
    TODO: 	1.suburb;
    		2.css;
    		3.POST;

--- WebContent/js/register.js
+++ WebContent/js/register.js
@@ -1,14 +1,38 @@
+var pwdLength = false;
+var pwdCfm = false;
+
 $(document).ready(function(){
 	
-	$("#submitbutton").bind("click", function() {
-		
-		var password = $("#password").val();
-		var passwordcfm = $('#passwordcfm').val();
-		
-		if (password != passwordcfm) {
-			alert("wrong password confirm");
+	$('#password').on('keyup', function() {
+		if (this.value.length >= 8) {
+			$('#password').css('background-color', 'initial');
+			pwdLength = true;
 		} else {
+			$('#password').css('background-color', '#FFAFA1');
+			pwdLength = false;
+		}
+	});
+	
+	$('#rpassword').on('keyup', function() {
+		if ($(this).val() == $('#password').val()) {
+			$('#rpassword').css('background-color', 'initial');
+			pwdCfm = true;
+		} else {
+			$('#rpassword').css('background-color', '#FFAFA1');
+			pwdCfm = false;
+		}
+	});
+	
+	$("#submit").bind("click", function() {
+		
+		var msg = "";
+		
+		if (pwdLength && pwdCfm) {
 			$("#registerform").submit();
+		} else if (!pwdLength) {
+			alert("The password must be at least 8 characters.");
+		} else if (!pwdCfm) {
+			alert("The password confirmation does not match.");			
 		}
 		
 	});
--- src/elec3610/servlet/RegisterServlet.java
+++ src/elec3610/servlet/RegisterServlet.java
@@ -1,36 +1,65 @@
 package elec3610.servlet;
-  
+
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Properties;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest; import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class RegisterServlet extends HttpServlet { 
-	
+
+public class RegisterServlet extends HttpServlet {
+
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException {
-
-		String username = request.getParameter("username"); 
-		String password = request.getParameter("password"); 
-		String passwordcfm = request.getParameter("passwordcfm"); 
-		String gender = request.getParameter("gender");
-		String year = request.getParameter("year");
 		
-        // response to the client
+		String username = request.getParameter("username");
+		System.out.println("username=" + username);
+		
+		String password = request.getParameter("password");
+		System.out.println("password=" + password);
+		
+		String passwordcfm = request.getParameter("passwordcfm");
+		System.out.println("passwordcfm=" + passwordcfm);
+		
+		String gender = request.getParameter("gender");
+		System.out.println("gender=" + gender);
+		
+		String year = request.getParameter("year");
+		System.out.println("year=" + year);
+		
+		// add the data to the server
 		try {
-			PrintWriter out = response.getWriter();
-			response.setContentType("text;html;charset=utf-8"); 
-			out = response.getWriter();
-			out.print("this is the response from the server\n"); 
-			out.print("password:" + password+ "\n"); 
-			out.print("gender:" + gender+ "\n"); 
-			out.print("year:" + year+ "\n");
+//			Class.forName("com.mysql.jdbc.Driver");
+//			String mysqlUrl = "jdbc:mysql://localhost:3306/elec3610";
+//			Properties userInfo = new Properties();
+//			userInfo.put("user", "root");
+//			userInfo.put("password", "root");
+//			Connection connection = DriverManager.getConnection(mysqlUrl,userInfo);
+//			Statement stmt = connection.createStatement();
+//			String sql = "INSERT INTO user VALUES (null, '"+username+"', '"+password+"', "+gender+", "+year+")";
+//	        stmt.executeUpdate(sql);
+		       
+	        // response to the client
+	        PrintWriter out = response.getWriter();
+	        response.setContentType("text;html;charset=utf-8");
+	        out = response.getWriter();
+			out.print("You have inserted a new user");
+
 		} catch (Exception e) {
-                e.printStackTrace();
-        }
+			e.printStackTrace();
+		}
+		
+		
 	}
-	protected void doPost(HttpServletRequest request, 
+
+	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException {
 		doGet(request, response);
 	}
+
 }
